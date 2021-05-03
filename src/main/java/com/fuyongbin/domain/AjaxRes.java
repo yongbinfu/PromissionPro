b335fc61f023d59678547349a508e45f50825a3d
fyb <851070420@qq.com>
2021-05-04 00:15:59 +0800

    add 信息

--- src/main/java/com/fuyongbin/domain/AjaxRes.java
+++ src/main/java/com/fuyongbin/domain/AjaxRes.java
@@ -3,7 +3,7 @@ package com.fuyongbin.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
+/*信息*/
 @Getter@Setter@ToString
 public class AjaxRes {
     private Boolean success;
